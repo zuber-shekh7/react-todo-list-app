3f36e148db56c08493e4a14ace786d190ca79b9d
Zuber Shekh <zuber.shekh7@gmail.com>
2023-06-29 21:50:34 +0530

    add react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <main>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </main>
+  );
 };
 
 export default App;
